259eb7f656c24134078347e021c86099d1ace7b8
deandradelucas <147111325+deandradelucas@users.noreply.github.com>
2026-04-09 21:23:31 -0300

    0

--- src/lib/brandAssets.js
+++ src/lib/brandAssets.js
@@ -2,7 +2,7 @@ export const BRAND_ASSETS = {
   /** Alias usado em telas como BemVindoAssinatura */
   logo: '/images/horizonte_fiel_original_logo_dark.svg',
   logoOnDark: '/images/horizonte_fiel_original_logo_dark.svg',
-  logoOnLight: '/images/horizonte_fiel_original_logo_light.svg',
+  logoOnLight: '/images/horizonte_fiel_original_logo_light.png',
   iconOnDark: '/images/horizonte_fiel_original_icon_light.svg',
   appIcon: '/images/horizonte_fiel_original_icon_dark.svg',
 }
